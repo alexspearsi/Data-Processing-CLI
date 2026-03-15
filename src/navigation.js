fcde1e232197e830a9ac8a915484039272b5bd1e
Alex <strelchenko.aleksandr@yandex.ru>
2026-03-15 19:40:06 +0200

    feat: implement count command

--- src/navigation.js
+++ src/navigation.js
@@ -1,5 +1,6 @@
-import * as path from 'node:path';
 import * as fs from 'node:fs/promises';
+import { resolvePath } from './commands/utils/pathResolver.js'
+import { count } from './commands/count.js'
 
 export async function navigation(input, currentDir) {
   const [command, ...args] = input.trim().split(' ');
@@ -14,15 +15,14 @@ export async function navigation(input, currentDir) {
     case 'ls':
       return await ls(currentDir)
 
+    case 'count':
+      return await count(currentDir, args[0], args[1])
+
     default:
       console.log('No such option');
-      return { dir: currentDir, success: false }
+      return { directory: currentDir, success: false }
   }
 }
-
-
-
-
 
 export function up(currentDir) {
   return path.resolve(currentDir, '..')
@@ -35,9 +35,7 @@ export async function cd(currentDir, targetPath) {
     return { directory: currentDir, success: false };
   }
 
-  const newPath = path.isAbsolute(targetPath)
-    ? targetPath
-    : path.resolve(currentDir, targetPath);
+  const newPath = resolvePath(currentDir, targetPath)
 
   try {
     const stat = await fs.stat(newPath);
